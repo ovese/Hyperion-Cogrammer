28bfd01a75e45da590341adddd96eb1d8b0c98e6
ovese <oveseovese@gmail.com>
2023-12-31 19:24:11 +0100

    saved challenge

--- optional_challenge.py
+++ optional_challenge.py
@@ -1,5 +1,5 @@
-""" 
-this challenge tests 
+"""
+this challenge tests
 2 compilation errors
 1 runtime error
 1 logical error
@@ -7,12 +7,13 @@ this challenge tests
 # print(f"Welcome {my_name} to this challenge")
 # on line 7 I try to print a variable that was not defined prior
 
-# the list below has contents firstname, last name, age, street, postcoe and city
+# the list below has contents firstname,
+# last name, age, street, postcoe and city
 my_list = ["Ese", "ovie", "40", "Concord", "NE33 9QB", "Newcastle"]
 
 for item in my_list:
     print(item)
-    
+
 list_size = len(my_list)
 print(f"The size of the list is: {list_size}")
 print(f"Twice my curent age is: {my_list[3]*2}")
@@ -24,7 +25,8 @@ print(f"Twice my current age is: {my_list[2]*2}")
 new_list = []
 new_list = my_list.append("email")
 print(new_list)
-# i have tried creating new list and assigning an expanded original list to it wrongly
+# i have tried creating new list and assigning
+# an expanded original list to it wrongly
 
 new_list1 = []
 my_list.append("email")
@@ -34,6 +36,6 @@ print(new_list1)
 another_list = []
 another_list.append("email")
 for i in another_list:
-    print(item) # another logical error printing the wrong variable item instead of i
-
-
+    print(item)
+    # another logical error printing the
+    # wrong variable item instead of i
